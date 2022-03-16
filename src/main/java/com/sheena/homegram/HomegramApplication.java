ed5a213a3238e59e8879c33a74fff6b1e6677d3f
sheena <jinyinshu90@gmail.com>
2022-03-16 10:45:38 +0900

    [김은숙] - 최초 파일 추가

---
+++ src/main/java/com/sheena/homegram/HomegramApplication.java
@@ -0,0 +1,13 @@
+package com.sheena.homegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheena/homegram/HomegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheena.homegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
